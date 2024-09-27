adbda559036cc650fb0976f130b04c41f5692dd3
Rohant <villarosarohant@gmail.com>
2024-09-27 17:38:40 +0530

    landing page

--- components/featuredProject.js
+++ components/featuredProject.js
@@ -1,41 +1,39 @@
 const projects = [
   {
-    imgSrc:
-      "https://lh3.google.com/u/0/d/15ouEgBRCaSs2Mf1p3S4T4fjGEru6PhXH=w2000-h1740-iv1",
+    videoSrc:
+      "https://spfiles.sfo3.cdn.digitaloceanspaces.com/sfweb/nikekids/SF_WORK_NIKE%20KIDS_E.mp4#t=1",
     name: "Project 1",
   },
   {
-    imgSrc:
-      "https://lh3.google.com/u/0/d/15FTTIlMKGVnQnznQrVAMqGuBZjbFpUXa=w1920-h927-iv1",
+    videoSrc:
+      "https://spfiles.sfo3.cdn.digitaloceanspaces.com/sfweb/arctic/SF_WORK_Adventures_C.mp4#t=1",
     name: "Project 2",
   },
   {
-    imgSrc:
-      "https://lh3.google.com/u/0/d/1_OsQ4O97-l119zXbQWUJCuKecaBNJl8_=w1172-h907-iv1",
+    videoSrc:
+      "https://spfiles.sfo3.cdn.digitaloceanspaces.com/sfweb/nikekids/SF_WORK_NIKE%20KIDS_I.mp4#t=1",
     name: "Project 3",
   },
   {
-    imgSrc:
-      "https://lh3.google.com/u/0/d/1x6ONgTRY4jaouANOzaYqBFqTGV-lQXV9=w1920-h927-iv1",
+    videoSrc:
+      "https://spfiles.sfo3.cdn.digitaloceanspaces.com/sfweb/nikekids/SF_WORK_NIKE%20KIDS_A.mp4#t=1",
     name: "Project 4",
   },
 ];
 
 function loadFeaturedProjects() {
-  // Get the work-set container
   const workSetContainer = document.getElementById("work-set");
 
   if (!workSetContainer) {
     return;
   }
 
-  // Build the HTML string
   let projectHTML = "";
 
   projects.forEach((project) => {
     projectHTML += `
         <div class="set">
-        <img src="${project.imgSrc}" alt="${project.name}">
+        <video autoplay loop muted src="${project.videoSrc}" alt="${project.name}"></video>
         <div class="work-details">
         <h6>${project.name}</h6>
         </div>
@@ -43,7 +41,6 @@ function loadFeaturedProjects() {
         `;
   });
 
-  // Add the generated HTML to the container's innerHTML
   workSetContainer.innerHTML = projectHTML;
 }
 
